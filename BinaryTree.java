963a59612fd54eca014192df261aa40a8363da95
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2023-10-22 21:57:42 +0200

    BinaryTree implemented untill the contains method

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class BinaryTree<T> {
     BinaryTreeNode<T> root;
 
@@ -5,5 +9,46 @@ public class BinaryTree<T> {
         return root;
     }
 
+    public void setRoot(BinaryTreeNode<T> root){
+        this.root = root;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public int size() {
+        if (isEmpty()) return 0;
+
+        int counter = 0;
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            var node = stack.pop();
+            counter++;
+            if(node.getLeftChild() != null) stack.push(node.getLeftChild());
+            if (node.getRightChild() != null) stack.push(node.getRightChild());
+        }
+        return counter;
+    }
+    public boolean contains(T element){
+        if (isEmpty()) return false;
+
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            var current = stack.pop();
+            if(current.getElement().equals(element)) return true;
+            if(current.getLeftChild() != null) stack.push(current.getLeftChild());
+            if(current.getRightChild() != null) stack.push(current.getRightChild());
+        }
+        return false;
+    }
+
     
+
+
+
 }
