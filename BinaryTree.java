bfe944098f97c1cae33efabe24e82cdcc3f377d6
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2023-10-27 14:16:22 +0200

    Traversals implemented

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,5 @@
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
@@ -47,8 +49,88 @@ public class BinaryTree<T> {
         return false;
     }
 
-    
+    public ArrayList<T> inOrder(){
+        ArrayList<T> result = new ArrayList<>();
+        if(isEmpty()) return result;
 
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        var current = root;
+
+        while(current != null || !stack.isEmpty()){
+            while(current != null){
+                stack.push(current);
+                current = current.getLeftChild();
+            }
+            current = stack.pop();
+            result.add(current.getElement());
+            current = current.getRightChild();
+        }
+        return result;
+    }
+    public ArrayList<T> preOrder(){
+        ArrayList<T> result = new ArrayList<>();
+        if(isEmpty()) return result;
+
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            var current = stack.pop();
+            result.add(current.getElement());
+            if(current.getRightChild() != null) stack.push(current.getRightChild());
+            if(current.getLeftChild() != null) stack.push(current.getLeftChild());
+        }
+        return result;
+    }
+    public ArrayList<T> postOrder(){
+        ArrayList<T> result = new ArrayList<>();
+        if(isEmpty()) return result;
+
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        var current = root;
+
+        while(current != null || !stack.isEmpty()){
+            if(current!= null){
+                stack.push(current);
+                result.add(0, current.getElement());
+                current = current.getRightChild();
+        } else {
+                current = stack.pop();
+                current = current.getLeftChild();
+            }
+        }
+        return result;
+    }
+    public ArrayList<T> levelOrder(){
+        ArrayList<T> result = new ArrayList<>();
+        if (isEmpty()) {
+            return result;
+        }
+
+        Stack<BinaryTreeNode<T>> stack1 = new Stack<>();
+        Stack<BinaryTreeNode<T>> stack2 = new Stack<>();
+        stack1.push(root);
+
+        while (!stack1.isEmpty()) {
+            var current = stack1.pop();
+            result.add(current.getElement());
+
+            if (current.getRightChild() != null) {
+                stack2.push(current.getRightChild());
+            }
+            if (current.getLeftChild() != null) {
+                stack2.push(current.getLeftChild());
+            }
+
+            if (stack1.isEmpty()) {
+                Stack<BinaryTreeNode<T>> temp = stack1;
+                stack1 = stack2;
+                stack2 = temp;
+            }
+        }
+
+        return result;
+    }
 
 
 }
